547b778c4320102f65af6bb24a773926a14b6090
Farid Moreno <farid.leonardo98@gmail.com>
2026-04-07 21:56:31 -0500

    feat: add suspense in booking form in page

--- app/reservar/page.tsx
+++ app/reservar/page.tsx
@@ -1,13 +1,16 @@
-import { Navbar } from "@/components/navbar"
-import { BookingForm } from "@/components/booking-form"
-import { WhatsAppButton } from "@/components/whatsapp-button"
+import { Suspense } from "react";
+import { Navbar } from "@/components/navbar";
+import { BookingForm } from "@/components/booking-form";
+import { WhatsAppButton } from "@/components/whatsapp-button";
 
 export default function ReservarPage() {
   return (
     <main>
       <Navbar showBanner={false} />
-      <BookingForm />
+      <Suspense fallback={<div>Loading...</div>}>
+        <BookingForm />
+      </Suspense>
       <WhatsAppButton />
     </main>
-  )
+  );
 }
